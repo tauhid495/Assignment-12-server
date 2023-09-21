f0918da205af754a1c228418276c9929ae046684
tauhid495 <96792668+tauhid495@users.noreply.github.com>
2023-09-22 04:58:10 +0600

    'update'

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9zmi.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.g9zmi.mongodb.net/?retryWrites=true&w=majority`; 
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
